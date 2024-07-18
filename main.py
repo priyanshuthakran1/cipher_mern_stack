1752f7196ea4ea47583f17fc5c6f961e4d633110
priyanshuthakran1 <priyanshuthakran9410@gmail.com>
2024-07-18 16:00:59 +0530

    Update

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ def clone_repo(repo_url):
     except subprocess.CalledProcessError as e:
         print(f"Failed to clone repository '{repo_url}': {e}")
 
-def push():
+def push(current_repo_path):
+    subprocess.run(['git', 'remote', 'set-url', 'origin', current_repo_path], check=True)
     subprocess.run(['git', 'add', '.'])
     subprocess.run(['git', 'commit', '-m', 'Update'])
-    subprocess.run(['git', 'remote', 'add', 'https://github.com/priyanshuthakran1/cipher_mern_stack.git'])
     subprocess.run(['git', 'push', '-u', 'origin','main'])
     print("Repository pushed successfully.")
 
 
-urls = ['https://github.com/priyanshuthakran1/Lecture-51-CipherSchools-P-B']
-
+urls = ['https://github.com/priyanshuthakran1/Lecture-49-CipherSchools']
 # for url in urls:
 #     clone_repo(url)
-push()
+current_repo_path = 'https://github.com/priyanshuthakran1/cipher_mern_stack.git'
+push(current_repo_path)
