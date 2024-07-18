68fa82173ee4ba84e46d540066a1110fc9216c40
priyanshuthakran1 <priyanshuthakran9410@gmail.com>
2024-07-18 16:16:39 +0530

    Update

--- main.py
+++ main.py
@@ -5,32 +5,29 @@ def clone_repo(repo_url):
         print(f"Repository '{repo_url}' cloned successfully.")
     except subprocess.CalledProcessError as e:
         print(f"Failed to clone repository '{repo_url}': {e}")
-
-<<<<<<< HEAD
 def push(current_repo_path):
-    subprocess.run(['git', 'remote', 'set-url', 'origin', current_repo_path], check=True)
-    subprocess.run(['git', 'add', '.'])
-    subprocess.run(['git', 'commit', '-m', 'Update'])
-    subprocess.run(['git', 'push', '-u', 'origin','main'])
-    print("Repository pushed successfully.")
+    try:
+        # Set the remote URL
+        subprocess.run(['git', 'remote', 'set-url', 'origin', current_repo_path], check=True)
+        
+        # Add changes to staging
+        subprocess.run(['git', 'add', '.'], check=True)
+        
+        # Commit the changes
+        subprocess.run(['git', 'commit', '-m', 'Update'], check=True)
+        
+        # Push the changes to the remote repository
+        subprocess.run(['git', 'push', '-u', 'origin', 'main'], check=True)
+        
+        print("Repository pushed successfully.")
+    
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred: {e}")
 
 
-urls = ['https://github.com/priyanshuthakran1/Lecture-49-CipherSchools']
-# for url in urls:
-#     clone_repo(url)
-current_repo_path = 'https://github.com/priyanshuthakran1/cipher_mern_stack.git'
-push(current_repo_path)
-=======
-def push():
-    subprocess.run(['git', 'add', '.'])
-    subprocess.run(['git', 'commit', '-m', 'Update'])
-    subprocess.run(['git', 'push', 'origin','main'])
-    print("Repository pushed successfully.")
-
-
-urls = ['https://github.com/priyanshuthakran1/Lecture-51-CipherSchools-P-B']
-
+urls = ['https://github.com/priyanshuthakran1/Lecture-51-CipherSchools']
 for url in urls:
     clone_repo(url)
-    push()
->>>>>>> e2888049beb5540e40ff593a39aa6b3102a2db91
+current_repo_path = 'https://github.com/priyanshuthakran1/cipher_mern_stack.git'
+push(current_repo_path)
+
